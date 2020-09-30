a40478d5d5e4817f9ea8e70a38aabc7eca510b1b
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-30 17:38:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,6 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-     def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     student_cards = doc.css(".student-card a")
