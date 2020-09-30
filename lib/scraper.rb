e8cd9c2d838ee8985fb8a9539a4ad8fb993b036f
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-30 17:21:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student = [ ]
+    html = Nokogiri::HTML(open(index_url))
+  
   end
 
   def self.scrape_profile_page(profile_url)
