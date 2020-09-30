3eb9cd6af66da18e1bbc0eb89146b523ece8a7ee
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-30 17:25:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student = [ ]
+    students = [ ]
     html = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
   
   end
